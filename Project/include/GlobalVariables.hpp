5fbbebe52ba932d58e7fbc414a6c81d2f8cca0ec
rrasp94 <ruraspud@vsite.hr>
2025-09-06 21:29:58 +0200

    ispravak

--- Project/include/GlobalVariables.hpp
+++ Project/include/GlobalVariables.hpp
@@ -3,8 +3,12 @@
 #include "SFML/System/Vector2.hpp"
 
 namespace global {
-	constexpr sf::Vector2f MAP_SIZE  = sf::Vector2f(1200.0f, 600.0f);
-	constexpr sf::Vector2f NODE_SIZE = sf::Vector2f(20.0f, 20.0f);
+    
+    const sf::Vector2f MAP_SIZE = sf::Vector2f(1200.0f, 600.0f);
+    const sf::Vector2f NODE_SIZE = sf::Vector2f(20.0f, 20.0f);
 
-	constexpr sf::Vector2u NODE_COUNT = sf::Vector2u(MAP_SIZE.x / NODE_SIZE.x, MAP_SIZE.y / NODE_SIZE.y);
+    const sf::Vector2u NODE_COUNT = sf::Vector2u(
+        static_cast<unsigned int>(MAP_SIZE.x / NODE_SIZE.x),
+        static_cast<unsigned int>(MAP_SIZE.y / NODE_SIZE.y)
+    );
 }
--- Project/include/QuadMap.hpp
+++ Project/include/QuadMap.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-
 #include "SFML/Graphics/Vertex.hpp"
 #include "SFML/Graphics/Texture.hpp"
 #include "SFML/Graphics/RenderStates.hpp"
@@ -10,30 +9,25 @@
 
 class QuadMap {
 public:
-
-	void Create(bool createGrid = true);
-
-	void Render(priv::Renderer& renderer);
-
-	void Reset();
-
-	void Set(sf::Vector2u coords, sf::Color color);
-
-	void SetPosition(sf::Vector2f position);
-
-	sf::Vector2f GetSize() const;
+    void Create(bool createGrid = true);
+    void Render(priv::Renderer& renderer);
+    void Reset();
+    void Set(sf::Vector2u coords, sf::Color color);
+    void SetPosition(sf::Vector2f position);
+    sf::Vector2f GetSize() const;
 
 private:
-	constexpr static sf::Color s_defaultColor = sf::Color(0, 0, 0, 0);
 
-	std::vector<sf::Vertex> m_Vertices;
-	std::vector<sf::Vertex> m_GridVertices;
-	sf::Vector2f m_quadSize;
-	sf::Vector2u m_size;
-	sf::Vector2f m_position;
+    static const sf::Color s_defaultColor;
 
-	static sf::Texture s_defaultTexture;
+    std::vector<sf::Vertex> m_Vertices;
+    std::vector<sf::Vertex> m_GridVertices;
+    sf::Vector2f m_quadSize;
+    sf::Vector2u m_size;
+    sf::Vector2f m_position;
 
-	void CreateQuad(uint32_t x, uint32_t y, uint32_t index);
-	void CreateGrid();
+    static sf::Texture s_defaultTexture;
+
+    void CreateQuad(uint32_t x, uint32_t y, uint32_t index);
+    void CreateGrid();
 };
